557d19d6401dfc3110212324461777d8bf67347a
Nikolas Tapia <tapia@wias-berlin.de>
2021-03-30 11:50:57 +0200

    PyPI

--- setup.py
+++ setup.py
@@ -6,9 +6,5 @@ from setuptools_rust import RustExtension, Binding
 
 
 setup(
-    name="o3iss",
-    version="0.1.1",
     rust_extensions=[RustExtension("o3iss.o3iss", binding=Binding.RustCPython)],
-    packages=["o3iss"],
-    zip_safe=False
 )
