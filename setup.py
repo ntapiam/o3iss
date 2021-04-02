57372887f83b0bba9d33d069738bf3ecbdf43e2d
Nikolas Tapia <tapia@wias-berlin.de>
2021-04-02 12:00:40 +0200

    Python Setup

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from setuptools import setup
 from setuptools_rust import RustExtension, Binding
 setup(
-    name="iss",
-    author="Nikolas Tapia",
-    author_email="tapia@wias-berlin.de",
-    version="0.1.2",
     packages=["iss"],
-    zip_safe=False,
-    url="https://github.com/ntapiam/o3iss",
     rust_extensions=[RustExtension("iss.o3iss", binding=Binding.RustCPython)]
 )
