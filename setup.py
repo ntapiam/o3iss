de075143b1d9b052505e6d3f79f0da7c84885833
Nikolas Tapia <tapia@wias-berlin.de>
2021-04-02 10:18:08 +0200

    build and travis CI

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 from setuptools_rust import RustExtension, Binding
-
-
 setup(
-    packages = ["o3iss"],
-    rust_extensions = [RustExtension("o3iss.o3iss", binding=Binding.RustCPython)]
+    name="iss",
+    author="Nikolas Tapia",
+    author_email="tapia@wias-berlin.de",
+    version="0.1.2",
+    packages=["iss"],
+    zip_safe=False,
+    url="https://github.com/ntapiam/o3iss",
+    rust_extensions=[RustExtension("iss.o3iss", binding=Binding.RustCPython)]
 )
-
